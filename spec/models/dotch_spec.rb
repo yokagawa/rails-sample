67f3f5118e93e46e2572fab8d550fe206a350467
yuya okagawa <yokagawa.dev@gmail.com>
2016-04-05 01:40:04 +0900

    houndci指摘修正

--- spec/models/dotch_spec.rb
+++ spec/models/dotch_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Dotch, type: :model do
   describe "バリデーション" do
     it "サンプル" do
       dotch = create(:dotch)
-      expect(dotch.id).to eq(1);
+      expect(dotch.id).to eq(1)
     end
   end
 end
