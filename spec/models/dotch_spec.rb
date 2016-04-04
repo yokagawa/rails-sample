22a15bdc08ea6071a349d06329d130385ca058f3
yuya okagawa <yokagawa.dev@gmail.com>
2016-04-05 01:30:55 +0900

    houndci指摘対応

--- spec/models/dotch_spec.rb
+++ spec/models/dotch_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Dotch, type: :model do
   describe "バリデーション" do
     it "サンプル" do
-        dotch = create(:dotch)
+      dotch = create(:dotch)
     end
   end
 end
