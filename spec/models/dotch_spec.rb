6aedb3c3bde9710d08709915a0b76d64b61f2724
yuya okagawa <yokagawa.dev@gmail.com>
2016-04-05 01:35:23 +0900

    テスト失敗サンプル

--- spec/models/dotch_spec.rb
+++ spec/models/dotch_spec.rb
@@ -2,8 +2,9 @@ require "rails_helper"
 
 RSpec.describe Dotch, type: :model do
   describe "バリデーション" do
-    it "サンプル" do
+      it "サンプル" do
       dotch = create(:dotch)
+      expect(dotch.id).to eq(2);
     end
   end
 end
